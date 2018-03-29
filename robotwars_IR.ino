378b11cd85b39fc2090514b1c272af2c5d7aac01
rubenteck <32707826+rubenteck@users.noreply.github.com>
2018-03-29 09:32:18 +0200

    distance censor + IR code

---
+++ robotwars_IR.ino
@@ -0,0 +1,24 @@
+#define trigPin 10
+#define echoPin 9
+#define IRlight 13 //already defined
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  pinMode(IRlight, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 2) {  // change 4 to max range of IR light
+    digitalWrite(IRlight,HIGH);
+    delay(10000);
+    digitalWrite(IRlight, LOW);
+  }
+}
